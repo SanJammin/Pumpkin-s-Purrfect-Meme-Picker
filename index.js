a29ee06c43e13745220affe27e7222df6fa783a4
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-02-10 21:00:21 +0000

    Clean up for loop using for of loop

--- index.js
+++ index.js
@@ -167,14 +167,12 @@ const catsData = [
         image: "sad.gif",
         alt: "A cat looking sad",
     }
-]
+];
 
 function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
-        }
-    }
-console.log(emotionsArray)
-}
+    for (let cat of cats) {
+        for (let emotionTag of cat.emotionTags) {
+
+        };
+    };
+};
